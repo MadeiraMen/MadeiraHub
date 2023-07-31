007bef0c41b7fb309bc95cf167f5cdf88bdee078
MadeiraMen <141030437+MadeiraMen@users.noreply.github.com>
2023-07-31 21:23:46 +1000

    Add files via upload

---
+++ newsscript.js
@@ -0,0 +1,30 @@
+const content = [
+    "Munchtime Men BAMBOOZLED by bin that was found on top of a roof",
+    "Mozz suspects he was FIRED WITHOUT WARNING as his brand new key-card STOPS working",
+    "Laf Dog UNIMPRESSED with N. Hugo finding the bin funny, gives him an INSTANT Saturday detention (source needed.)",
+    "Spencer Hurley BEFFUDDLED as he witnesses Liam getting stuck at East Richmond Station at 3:30am",
+    "Jacob LAUNCHES music career with 4 singles going STRAIGHT to the top OF THE CHARTS (he also immediately announced his retirement as he doesn't have the sorts of riches that Zalek does)",
+    "GDP (Gavin Pinto) comes in CLUTCH as Mozz and Liam STRUGGLE to finish a level of Spot the Difference Daily, DISCOVERING the last difference which was an ABSOLUTE CURLER",
+    `Adam DECAPITATES Lysterfield Wolves player in ABHORRENT display of sportsmanship: "same old Blackburn players, always cheating" - expected response from average Lysterfield Wolves supporter.`,
+    "Liam WAGS day of school",
+    "Liam WAGS day of school",
+    "Liam WAGS day of school",
+    "Liam WAGS day of school",
+    `Production on 'The Famous 2023 Donvale Media Studio Hostage Situation' GRINDS to a halt, project gets CANNED after one day of shooting (and 3 terms of planning)`,
+    "Schoolwide DEPRESSION hits as players CANNOT use the Bigga-Ball.4",
+    "Matildas FUMBLE THE BAG after CONCEDING 3 straight goals against Nigeria, finally answering the question that yes, Kerr DID carry the team",
+    "INCREDIBLE, INSPIRING, IMPRESSIVE, INTRUGING, and INTERESTING story unfolds on school campus, but DOES NOT make the newsletter as Liam WASN'T THERE to see it",
+    "also, the Madeira Hub opened"
+];
+
+const orderedList = document.createElement("ol");
+
+content.forEach(itemText => {
+    const listItem = document.createElement("li");
+    
+    listItem.textContent = itemText;
+    orderedList.appendChild(listItem);
+});
+
+const listContainer = document.getElementById("list-container");
+listContainer.appendChild(orderedList);
---
+++ wordlescript.js
@@ -0,0 +1,103 @@
+const gridContainer = document.getElementById('grid-container');
+const numSquares = 30; // Number of squares (4x4 grid in this example)
+
+let squaresList = [];
+
+for (let i = 0; i < numSquares; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  gridContainer.appendChild(square);
+
+  squaresList[i] = square;
+}
+
+const wordToGuess = 'CHALK';
+let guessAmount = 0;
+
+let gameOver = false;
+
+function checkGuess() {
+
+if(gameOver=== true) {
+  return;
+}
+
+  const guessInput = document.getElementById('guess-input');
+  const guess = guessInput.value.toUpperCase();
+  guessInput.value = '';
+
+  if (guess.length !== 5 || !/^[A-Z]+$/.test(guess)) {
+    alert('Please enter a 5-letter word containing only uppercase letters.');
+    return;
+  }
+
+  const wordLetters = {}; // Store the occurrences of letters in wordToGuess
+  const guessLetters = {}; // Store the occurrences of letters in the guess
+
+  // Populate wordLetters and guessLetters
+  for (const letter of wordToGuess) {
+    wordLetters[letter] = (wordLetters[letter] || 0) + 1;
+  }
+
+  for (let i = 0; i < guess.length; i++) {
+    const letter = guess[i];
+    guessLetters[letter] = (guessLetters[letter] || 0) + 1;
+
+    const letterIndexInWord = wordToGuess.indexOf(letter);
+
+    const curSquare = squaresList[5 * guessAmount + i];
+    curSquare.innerHTML = letter;
+    curSquare.style.border = 'none';
+
+    if (letterIndexInWord === i) {
+      //Correct
+      curSquare.style.backgroundColor = 'green';
+    } else if (letterIndexInWord !== -1 && guessLetters[letter] <= wordLetters[letter]) {
+      //Correct but wrong position
+      curSquare.style.backgroundColor = 'orange';
+    } else {
+      //Incorrect
+      curSquare.style.backgroundColor = 'grey';
+    }
+  }
+
+  // Check if the player has won
+  if (guess === wordToGuess) {
+    document.getElementById('guess-form').style.display = 'none';
+
+    const resultScreen = document.getElementById('result-screen');
+    resultScreen.innerHTML = "CORRECT! YOU WON!";
+    resultScreen.style.display = 'block';
+    return;
+  }
+
+  guessAmount++;
+
+  if(guessAmount >= 6)
+  {
+    gameOver = true;
+    document.getElementById('guess-form').style.display = 'none';
+
+    const resultScreen = document.getElementById('result-screen');
+    resultScreen.innerHTML = "YOU SUCK!";
+    resultScreen.style.display = 'block';
+
+    setTimeout(() => {
+      window.location.replace("https://youtu.be/Wm_MHwsvTaQ?t=23");
+      return;
+    }, 2000);
+  }
+}
+
+const inputField = document.getElementById('guess-input');
+
+inputField.addEventListener('input', function () {
+  this.value = this.value.toUpperCase();
+});
+
+inputField.addEventListener('keyup', function(event) {
+  // Check if the key pressed is "Enter" (keyCode 13)
+  if (event.keyCode === 13) {
+    checkGuess();
+  }
+});
