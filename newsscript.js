ccd7fbcee590bd3cf67a6d97823c656aac602d4e
MadeiraMen <141030437+MadeiraMen@users.noreply.github.com>
2023-08-22 13:13:08 +1000

    Update newsscript.js

--- newsscript.js
+++ newsscript.js
@@ -1,16 +1,16 @@
 const content = [
-    "HEARTBREAK as The Hurn (Shannon Hurn) announces his retirement, somehow causing the Eagles to become EVEN WORSE",
-    "Toland is ordered to LEAVE the classroom after STINKING it out with a fart. Detective Crack believes this may be connected to the Kairos Stinker Incident, which claimed several lives last year.",
-    "Accounting ASTROCITY as class FLUNKS yet another SAC with a STAGGERING 55% average. One student, who will go unnamed, received a WHOPPING 13%",
-    "Newey ONCE AGAIN proves his IMMORALITY as he uses CHILDREN as examples of assets: “you would be depreciating in value” – possibly the last thing Kevy Dumb-Dumb said before he lost his teaching licence.",
-    "Liam is STRANDED after bus DIDN’T arrive, FORCING him to walk to school during a BLIZZARD* (source needed)",
-    "HUGE profit for Liam after purchasing ONE cheese and bacon toastie for the price of a regular cheese. He applauds Moule (Anthony M.) for his selfless humanitarian action.",
-    `John’s joyless journey after julienned potato snack is DROPPED on the GROUND, causing WIDESPREAD PANIC.`,
-    "HUGE allegation made on Dougy (Mr Douglas) and his questionable comments on sports: “I would rather be in the Matilda’s changing rooms”  - alleged quote from the now sketchy man (via Jordan Murtic)",
-    "BROBDINGNANGIAN reveal during accounting class as The News (Newey (Kevin Nguyen) CONFESSES his vaping addiction: “I Just cannot stop” – The News, on his unbreakable bond with Raspberry Mint",
-    "MASSIVE win for Aharro as he AQUIRES gold status in CSGO: “yeeeeeeeeeeeeeaaaaaaaaaaaaaaaaaaahhhhhhhhhhhh!!!!!!!!!!!” – Adam Harrison as he becomes SLIGHTLY less trash at the game.",
-    "National DELECTATION as Matildas BUTTFUCK the Danish Duds in an UNEQUIVICAL 2-0 THRASHING in the Round of 16",
-    `ANOTHER win for Adam Harrison as he guesses the DownBalle in one attempt (it was himself)`
+    "1.	The Tillies (Matildas) (Australian Women’s Soccer [football] Team) THRASH Denmark to YET AGAIN progress further in the World Cup (FIFA) Liam SUES Coles for forcing him to work during the shootout *source needed",
+    "Harro IMMESSURABLY angry at Lim for MISSING 3 quarters of SCINTILATING football, only being PRESENT for the last quarter with BK (Blake) (Keane) *source needed. Lysterfield Wolves players INVESTIGATED for ILLICIT use of PERFORMANCE-ENHANCNING supplements *source needed.",
+    "Gavin Pinto has A WORD OR TWO about an issue, and would LIKE to publicly share them: “I strongly support the side of the argument which my contention resides on” – Gavin Pinto on said issue",
+    "Mr Lafferty CRACKS IT at year 9’s and a few year 11’s (although the 11’s were fairly innocent) after kid was allegedly bullied on the chartered bus: “people who use the word snitch are COWARDS”  Mr Lafferty in his very deserved spray of the year 9’s.",
+    "Minh FUMBLES the Tetris Tournament, being ELIMINATED in the Prelims: “Tetris tournament, who ever thought of playing Tetris in a tournament? – Mr High",
+    "The Mr Hynes CRACKDOWN of 2023 as year eight class is given PERMANENT seating arrangement in F1:10",
+    `Study center DEFACED by poster-postering VANDALS with the wicked aim of replacing the whole wall`,
+    "Mr Wood FURIOUS as Liam does his maths in Mr Ward’s classroom instead, leaving Blake STRANDED back in S1.04",
+    "HUGE reveal as one of the year 8’s in Mr Hynes class was the PERUPTUATPER of Bus Incident (see entry 4) – name is Felix Petroulias ",
+    "Blake COMMENTS on the weather: “it’s quite warm today”¬ – Blake Keane on the apparent air temperature",
+    "Gavin UNIMPRESSED with BKeane’s lateness to class: “I am unimpressed” – GDP on Firemm waking up at 8:59am",
+    `Several members of the newsletter MILDLY IRRITATED as newsletter is NOT published on its scheduled friday`
 ];
 
 const orderedList = document.createElement("ol");
