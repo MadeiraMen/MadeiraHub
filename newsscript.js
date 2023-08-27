b493d96afd118959f627c17a44da288349ff2317
MadeiraMen <141030437+MadeiraMen@users.noreply.github.com>
2023-08-28 09:50:39 +1000

    Update newsscript.js

--- newsscript.js
+++ newsscript.js
@@ -1,16 +1,18 @@
 const content = [
-    "1.	The Tillies (Matildas) (Australian Women’s Soccer [football] Team) THRASH Denmark to YET AGAIN progress further in the World Cup (FIFA) Liam SUES Coles for forcing him to work during the shootout *source needed",
-    "Harro IMMESSURABLY angry at Lim for MISSING 3 quarters of SCINTILATING football, only being PRESENT for the last quarter with BK (Blake) (Keane) *source needed. Lysterfield Wolves players INVESTIGATED for ILLICIT use of PERFORMANCE-ENHANCNING supplements *source needed.",
-    "Gavin Pinto has A WORD OR TWO about an issue, and would LIKE to publicly share them: “I strongly support the side of the argument which my contention resides on” – Gavin Pinto on said issue",
-    "Mr Lafferty CRACKS IT at year 9’s and a few year 11’s (although the 11’s were fairly innocent) after kid was allegedly bullied on the chartered bus: “people who use the word snitch are COWARDS”  Mr Lafferty in his very deserved spray of the year 9’s.",
-    "Minh FUMBLES the Tetris Tournament, being ELIMINATED in the Prelims: “Tetris tournament, who ever thought of playing Tetris in a tournament? – Mr High",
-    "The Mr Hynes CRACKDOWN of 2023 as year eight class is given PERMANENT seating arrangement in F1:10",
-    `Study center DEFACED by poster-postering VANDALS with the wicked aim of replacing the whole wall`,
-    "Mr Wood FURIOUS as Liam does his maths in Mr Ward’s classroom instead, leaving Blake STRANDED back in S1.04",
-    "HUGE reveal as one of the year 8’s in Mr Hynes class was the PERUPTUATPER of Bus Incident (see entry 4) – name is Felix Petroulias ",
-    "Blake COMMENTS on the weather: “it’s quite warm today”¬ – Blake Keane on the apparent air temperature",
-    "Gavin UNIMPRESSED with BKeane’s lateness to class: “I am unimpressed” – GDP on Firemm waking up at 8:59am",
-    `Several members of the newsletter MILDLY IRRITATED as newsletter is NOT published on its scheduled friday`
+    "Yet ANOTHER teacher SACKED from Whitefriars as they COMPLIMENT Blake for his good looking balls: “nice ball, Blake” – Mr Payne on his last day of school.",
+    "DB (downball) SHATTERED as Bigga Ball v3 goes into drain a STAGGERING 3 times during the ONE day",
+    "Ollie chard CLAIMS he likes little fat boys: “I like little fat boys” – Ollie C",
+    "Jordan Walton (J-Wiz) DISTRAUGHT as he learns he has to do a SAC at school: “its unfair. No, that’s so unfair” – J-Wiz learning that he has to do a SAC to complete Unit 4",
+    "Lawrence and Dijkstra were CONVICTED of FRUAD after PILFERING Pinto and Keane’s Three Drink Theory: “We should have Copyrighted it” - GDP on the matter.",
+    "VCAL cohort REDEEMS themselves as sausage sizzle is hosted, becoming FAVOURABLE in the public eye",
+    `Andre Pauli STUNS downball members with INCREDIBLE debut, staying in king for a THOUSAND rounds *source needed`,
+    "Oscar Sherlock ROASTS student teacher the SECOND he enters the class: “why do you look like a goober” – oscar sherlock uttering his debut funny joke",
+    "BLEAK weekend for basketball as both Boomers and BKeane suffer CLOSE losses",
+    "Jacob MORTIFIED as he sees DEAD echidna that was RUN OVER by another driver",
+    "J-Dog the Falcon (Jacob Toland) declared DEAD at age 14 after not coming to school for WEEKS. Public celebrations ensue",
+    `J-Dog the Combo (Jacob Kingston) breaks aviation RECORD as he somehow exceeds terminal velocity after LEAPING off T4.01 (he had to attend several Mr Cotter classes)`
+    "Gavin APPROVES of Liam’s ILLICIT shoes worn to school: “those kicks are pretty fresh” – GDP regarding Liam’s foot fit"
+    "Mozz (Mr Zerella) INFORMS class that he was SICK, ILL and SOMEWHAT UNWELL ON THE WEEKEND."
 ];
 
 const orderedList = document.createElement("ol");
